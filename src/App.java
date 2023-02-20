0c4004accc90f3f16de1d100e80c3c5f33970700
Guilherme <64824790+guilhe100@users.noreply.github.com>
2023-02-20 00:58:26 -0300

    termino do projeto

--- src/App.java
+++ src/App.java
@@ -1,22 +1,51 @@
 import java.time.LocalDate;
 
+import dio.guilherme.model.Bootcamp;
+import dio.guilherme.model.Conteudo;
 import dio.guilherme.model.Curso;
+import dio.guilherme.model.Dev;
 import dio.guilherme.model.Mentoria;
 
 public class App {
     public static void main(String[] args) throws Exception {
         Curso curso1 = new Curso(); 
-        curso1.setNome("pROMAGA");
+        curso1.setTitulo("pROMAGA");
         curso1.setDescricao("Aprender a progamar");
         curso1.setCargaHoraria(200);
-
-        System.out.println(curso1);
 
         Mentoria mentoria = new Mentoria();
         mentoria.setTitulo("Mentoria de ajva");
         mentoria.setDescricao("uma mentoria");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(mentoria);
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp java Developer");        
+        bootcamp.setDescricao("Descrição Bootcamp java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devGuilherme = new Dev();
+        devGuilherme.setNome("Guilherme");
+        devGuilherme.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos inscritos: " + devGuilherme.getConteudoInscritos());
+        devGuilherme.progredir();
+        System.out.println("XP: " + devGuilherme.calcularTotalXp());
+        System.out.println("Conteudos inscritos: " + devGuilherme.getConteudoInscritos());
+        System.out.println("Conteudos Concluidos: " + devGuilherme.getConteudoConcluidos());
+
+        Dev devRichard = new Dev();
+        devRichard.setNome("Richard");
+        devRichard.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos inscritos: " + devRichard.getConteudoInscritos());
+        devRichard.progredir();
+        devRichard.progredir();
+
+        System.out.println("Conteudos inscritos: " + devRichard.getConteudoInscritos());
+        System.out.println("Conteudos Concluidos: " + devRichard.getConteudoConcluidos());
+        System.out.println("XP: " + devRichard.calcularTotalXp());
+
+        System.out.println();
+
+
     }
 }
--- src/dio/guilherme/model/Curso.java
+++ src/dio/guilherme/model/Curso.java
@@ -2,40 +2,24 @@ package dio.guilherme.model;
 
 import java.util.Calendar;
 
-public class Curso {
-    private String nome;
-    private String descricao;
+public class Curso extends Conteudo{
     private int cargaHoraria;
-
-    public String getNome() {
-        return nome;
-    }
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-    public String getDescricao() {
-        return descricao;
-    }
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
+   
     public int getCargaHoraria() {
         return cargaHoraria;
     }
     public void setCargaHoraria(int cargaHoraria) {
         this.cargaHoraria = cargaHoraria;
     }
-    
+
     @Override
     public String toString() {
-        return "Curso [nome=" + nome + ", descricao=" + descricao + ", cargaHoraria=" + cargaHoraria + "]";
+        return "Curso [titulo=" + getTitulo() + ", descricao=" + getDescricao() + cargaHoraria + "]";
     }
 
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO * cargaHoraria;
+    }
     
-
-    
-    
-
-
-
 }
